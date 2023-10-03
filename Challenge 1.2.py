995ca60711398b2f98300f77899dd85951d89873
Dhahirasherin123 <145670915+Dhahirasherin123@users.noreply.github.com>
2023-10-03 18:54:27 +0530

    Update Challenge 1.2.py

--- Challenge 1.2.py
+++ Challenge 1.2.py
@@ -3,6 +3,6 @@ year = int(input("Enter a year: "))
 
 # Check if it's a leap year
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-  print(f"{year} is a leap year.".format(year))
+  print(f"{year}is a leapyear.".format(year))
 else:
   print(f"{year} is not a leap year.".format(year))
